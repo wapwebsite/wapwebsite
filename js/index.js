f2fca2a087875223304202f425c3203692e3c8a3
Sharon Chen <sharonch3n@gmail.com>
2018-09-21 21:19:52 -0500

    commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("button[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} );
+});
